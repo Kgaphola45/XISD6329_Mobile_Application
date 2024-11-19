79e9d837a0aeeec27244cc655756b3f8d4970aa3
Emmanuel kgaphola <it45ma@protonmail.ch>
2024-11-19 02:09:18 +0200

    "Added JUnit test for SuccessAPI, including setup method and test for API request structure"

--- app/src/test/java/com/devcrafters/xisd6329/SuccessAPITest.java
+++ app/src/test/java/com/devcrafters/xisd6329/SuccessAPITest.java
@@ -1,4 +1,32 @@
 package com.devcrafters.xisd6329;
 
+import static org.junit.Assert.*;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class SuccessAPITest {
+
+    private Success success;
+    private String mockURL = "http://10.0.2.2/login/submit_appointment.php";
+
+    @Before
+    public void setup() {
+        success = new Success();
+    }
+
+    @Test
+    public void testAPIRequestStructure() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, mockURL, response -> {
+            assertNotNull("Response should not be null", response);
+        }, error -> {
+            fail("Request failed");
+        });
+
+        assertEquals("Request method should be POST", Request.Method.POST, stringRequest.getMethod());
+        assertEquals("URL should match", mockURL, stringRequest.getUrl());
+    }
 }
